b174bb91163db13a633a45564d195f840588d2f8
hauviptt97 <hauviptt@gmail.com>
2020-01-12 10:22:49 +0700

    lesson01 - code -07.01.2020

---
+++ lesson01-setup-environment/src/test/java/com/haubui/lesson01_sett/AppTest.java
@@ -0,0 +1,20 @@
+package com.haubui.lesson01_sett;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
